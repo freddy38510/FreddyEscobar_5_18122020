369377cd785d5ef9f3f2f3e57eb0ce54baa9e6cf
Freddy <freddyescobar@hotmail.fr>
2020-12-23 13:01:50 +0100

    feat: provide ability to add product to cart

--- src/js/controllers/product.js
+++ src/js/controllers/product.js
@@ -2,14 +2,18 @@ import Client from '../client';
 import Product from '../product';
 import ProductCategory from '../product-category';
 import ProductView from '../views/product';
-import { insertToDOM } from '../helpers';
+import Cart from '../cart';
+import { insertToDOM, injectTotalItemsCart, Notify } from '../helpers';
 
 export default class ProductController {
-  constructor(category = process.env.PRODUCT_CATEGORY) {
+  constructor(formId = 'product-form', category = process.env.PRODUCT_CATEGORY) {
     this.client = new Client();
     this.category = new ProductCategory(category);
     this.products = [];
     this.product = null;
+    this.formId = formId;
+
+    document.body.addEventListener('submit', this);
   }
 
   async index() {
@@ -85,3 +89,38 @@ export default class ProductController {
       }
     }
   }
+
+  onAddToCart(event) {
+    if (event.target.id !== this.formId) {
+      // submit event from an other form, resolve promise
+      return;
+    }
+
+    event.preventDefault();
+
+    try {
+      const product = Object.fromEntries(new FormData(event.target));
+
+      product.quantity = Number(1);
+
+      Cart.addProduct(product);
+
+      injectTotalItemsCart('.total-items-cart');
+
+      Notify('Produit ajouté au panier avec succès!', `#${this.formId}`);
+    } catch (error) {
+      Notify('Impossible d\'ajouter le produit au panier!', `#${this.formId}`, 'bg-danger text-white');
+
+      if (process.env.DEV) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    }
+  }
+
+  handleEvent(e) {
+    if (e.type === 'submit') {
+      this.onAddToCart(e);
+    }
+  }
+}
